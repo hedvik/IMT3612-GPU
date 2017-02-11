278268f3bb75e4c422e2b6e0bfdc2904086187f2
andrwan@stud.ntnu.no <andrwan@stud.ntnu.no>
2017-02-11 11:03:05 +0100

    Added project for the vertex buffers tutorial

---
+++ shader/Vertex Input/Vertex Input/Source.cpp
@@ -0,0 +1,34 @@
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+#include "VulkanAPIHandler.h"
+#include "consts.h"
+
+GLFWwindow* initWindow(int width, int height) {
+	glfwInit();
+
+	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+	glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+	auto window = glfwCreateWindow(width, height, "Vulkan", nullptr, nullptr);
+	return window;
+}
+
+void setupResizeCallback(GLFWwindow* window, VulkanAPIHandler &app) {
+	glfwSetWindowUserPointer(window, app.getPtr());
+	glfwSetWindowSizeCallback(window, VulkanAPIHandler::onWindowResized);
+}
+
+int main() {
+	auto window = initWindow(WINDOW_WIDTH, WINDOW_HEIGHT);
+	VulkanAPIHandler vulkanAPIHandler(window);
+	setupResizeCallback(window, vulkanAPIHandler);
+
+	while (!glfwWindowShouldClose(window)) {
+		glfwPollEvents();
+		vulkanAPIHandler.drawFrame();
+	}
+
+	glfwDestroyWindow(window);
+
+	return 0;
+}
